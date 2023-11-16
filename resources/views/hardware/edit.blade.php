034ddee1d938b57e18331bdde8314a77aa2123cd
richu <ruchebe@gmail.com>
2023-11-16 15:12:22 +0000

    please work

--- resources/views/hardware/edit.blade.php
+++ resources/views/hardware/edit.blade.php
@@ -110,18 +110,7 @@
             @include ('partials.forms.edit.warranty')
 
             <!-- byod checkbox -->
-            <div class="form-group">
-                <div class="col-md-7 col-md-offset-3">
-                    <label for="byod" class="form-control">
-                        <input type="checkbox" value="1" name="byod" {{ (old('remote', $item->byod)) == '1' ? ' checked="checked"' : '' }} aria-label="byod">
-                        {{ trans('general.byod') }}
-
-                    </label>
-                    <p class="help-block">{{ trans('general.byod_help') }}
-                    </p>
-                </div>
-            </div>
-        </div>
+      
     </div>
 
     <div class="form-group">
